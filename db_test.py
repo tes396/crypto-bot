64b11350f59de8fe88ad893b6f73eefbea405976
tes396 <tesu9589@gmail.com>
2026-02-26 23:07:52 +0900

    Create db_test.py

---
+++ db_test.py
@@ -0,0 +1,40 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute("SELECT version();")
+    version = cur.fetchone()
+    print("Connected to:", version)
+
+    cur.close()
+    conn.close()
+
+    print("SUCCESS: Database connection working")
+
+except Exception as e:
+    print("DB CONNECTION FAILED")
+    print(str(e))
+    raise
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
